9ea9c568072d2f32e92ccf59ec294ce63259d254
Zeniz <zeniz.sians@gmail.com>
2019-02-14 15:20:50 +0900

    skillbookUI is comming! imgs upload
    skillbookUI is comming! imgs upload

---
+++ skillbookUI.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "skillbookUI.h"
+
+
+skillbookUI::skillbookUI()
+{
+}
+
+
+skillbookUI::~skillbookUI()
+{
+}
---
+++ skillbookUI.h
@@ -0,0 +1,8 @@
+#pragma once
+class skillbookUI
+{
+public:
+	skillbookUI();
+	~skillbookUI();
+};
+
