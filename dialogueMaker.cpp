38613f70f2c858664c74864bf6362d7cb9ea027a
HyungwooYou <tlsehguddn23@gmail.com>
2019-02-15 17:11:58 +0900

    dialogueMaker
    dialogueMaker

---
+++ dialogueMaker.cpp
@@ -0,0 +1,105 @@
+#include "stdafx.h"
+#include "dialogueMaker.h"
+
+
+dialogueMaker::dialogueMaker()
+{
+}
+
+
+dialogueMaker::~dialogueMaker()
+{
+}
+
+HRESULT dialogueMaker::init()
+{
+	IMAGEMANAGER->addImage("chatBoxImg", "images/dialogueMaker/chatBox.bmp", 1440, 225, true, RGB(255, 0, 255));
+	_chatBoxImg = IMAGEMANAGER->findImage("chatBoxImg");
+
+	_text.clear();
+
+	_isStart = false;
+	_time = 0;
+	_printSpeed = 0;
+	_printLen = 0;
+	_printSpeed = 0.0f;
+	_text.append(" ");
+
+	return S_OK;
+}
+
+void dialogueMaker::release()
+{
+	//폰트가 실행됨
+	//폰트가 모두 진행됫다는걸 알려줌
+}
+
+void dialogueMaker::update()
+{
+	if (_isStart == true)
+	{
+		_time += TIMEMANAGER->getElapsedTime();
+		if (_time >= _printSpeed)
+		{
+			_time = 0;
+			if (_printLen < _text.size())
+			{
+				tmpChar = _text.at(_printLen);
+
+				if (0 <= tmpChar && tmpChar < 128)
+				{
+					_printLen += 1;
+				}
+				else
+				{
+					_printLen += 2;
+				}
+
+				if (KEYMANAGER->isOnceKeyDown(VK_RETURN) || KEYMANAGER->isOnceKeyDown('F'))
+				{
+					_printLen = _text.size();
+				}
+			}
+			else if (_printLen == _text.size())
+			{
+				if (KEYMANAGER->isOnceKeyDown(VK_RETURN) || KEYMANAGER->isOnceKeyDown('F'))
+				{
+					_isStart = false;
+					this->setDialogue(_npcName, _text, _printSpeed);
+				}
+			}
+		}
+	}
+}
+
+
+
+void dialogueMaker::render()
+{
+	if (_isStart == true)
+	{
+		HFONT font, oldFont;
+
+		font = CreateFont(30, 10, 0, 0, 0, 0, 0, 0, HANGUL_CHARSET, 0, 0, 0, 0, "쉐도우9");
+		oldFont = (HFONT)SelectObject(getMemDC(), font);
+
+		_chatBoxImg->alphaRender(getMemDC(), (WINSIZEX - 1440) / 2, WINSIZEY - 285, 250);
+
+		_npcPicture->render(getMemDC(), 100, WINSIZEY - 265);
+
+		TextOut(getMemDC(), 330, WINSIZEY - _chatBoxImg->GetHeight() + 50, _text.c_str(), _printLen);
+		SelectObject(getMemDC(), oldFont);
+		DeleteObject(font);
+	}
+}
+
+void dialogueMaker::setDialogue(string npcName, string text, float talkSpeed)
+{
+	_npcName = npcName;
+	_npcPicture = IMAGEMANAGER->findImage(_npcName.c_str());
+
+	_text = text;
+	_printSpeed = talkSpeed;
+	//_printLen = 0;
+	_isStart = false;	//	false고정
+}
---
+++ dialogueMaker.h
@@ -0,0 +1,52 @@
+#pragma once
+#include "gameNode.h"
+class dialogueMaker : public gameNode
+{
+private:
+	//채팅이미지
+	image* _chatBoxImg;
+
+	//받아올 Npc 이미지(이미지매니저에 추가되잇어야됨)
+	image* _npcPicture;
+	//받아올 Npc 키값
+	string _npcName;
+
+
+	//가져올 대사
+	string _text;
+
+	//시작할시
+	bool _isStart;
+
+
+	//TimeManger사용을 위한 변수들
+	float _time;
+	//출력속도
+	float _printSpeed;
+
+	//받아온 글자길이
+	int _printLen;
+
+	//아스키문자 비교용
+	char tmpChar;
+
+
+public:
+	dialogueMaker();
+	~dialogueMaker();
+
+	HRESULT init();
+	void release();
+	void update();
+	void render();
+
+	void setText(string text) { _text = text; }
+	void setPrintSpeed(float talkSpeed) { _printSpeed = talkSpeed; }
+
+
+	void setIsStart(bool isStart) { _isStart = isStart; }
+	//텍스트,폰트별속도,채팅시작
+	void setDialogue(string npcName, string text, float talkSpeed);
+	bool getisStart() { return _isStart; }
+};
+
