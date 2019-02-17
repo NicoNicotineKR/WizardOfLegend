b6b58dc001749cb01a358963d971c2246859cdd2
Zeniz <zeniz.sians@gmail.com>
2019-02-17 16:04:40 +0900

    bossProgressBarUI start
    bossProgressBarUI start

---
+++ bossHpProgressBarUI.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "bossHpProgressBarUI.h"
+
+
+bossHpProgressBarUI::bossHpProgressBarUI()
+{
+}
+
+
+bossHpProgressBarUI::~bossHpProgressBarUI()
+{
+}
---
+++ bossHpProgressBarUI.h
@@ -0,0 +1,8 @@
+#pragma once
+class bossHpProgressBarUI
+{
+public:
+	bossHpProgressBarUI();
+	~bossHpProgressBarUI();
+};
+
