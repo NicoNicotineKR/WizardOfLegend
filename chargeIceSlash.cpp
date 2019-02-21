1e0777acd53337c414ddf84459abe9ba618f874c
Zeniz <zeniz.sians@gmail.com>
2019-02-21 17:41:40 +0900

    skill charge Update!!!
    skill charge Update!!!

--- chargeIceSlash.cpp
+++ chargeIceSlash.cpp
@@ -10,3 +10,84 @@ chargeIceSlash::chargeIceSlash()
 chargeIceSlash::~chargeIceSlash()
 {
 }
+
+HRESULT chargeIceSlash::init()
+{
+	_slashNum = 0;
+	_destPos = { 0, 0 };
+	_vec = { 0,0 };
+	_angle = 0;
+
+	_skillTimer = 0;
+
+	_isStart = false;
+
+	_bossPos = nullptr;
+	_playerPos = nullptr;
+
+	return S_OK;
+}
+
+void chargeIceSlash::release()
+{
+	_isStart = false;
+	_bossPos = nullptr;
+	_playerPos = nullptr;
+	_slashNum = 0;
+
+	_skillTimer = 0.f;
+	_destPos.x = 0;
+	_destPos.y = 0;
+
+	_angle = 0.f;
+	_vec.x = 0.f;
+	_vec.y = 0.f;
+}
+
+void chargeIceSlash::update()
+{
+
+	if (_isStart) {
+		if (_skillTimer > END_TIME) {
+			_isStart = false;
+			this->release();
+			return;
+		}
+
+		_skillTimer += TIMEMANAGER->getElapsedTime();
+		
+
+		_bossPos->x += _vec.x;
+		_bossPos->y += _vec.y;
+
+	}
+
+}
+
+void chargeIceSlash::render()
+{
+}
+
+void chargeIceSlash::UseSkill(POINTFLOAT * bossPos, POINTFLOAT * playerPos, int slashNum)
+{
+	_isStart = true;
+	_bossPos = bossPos;
+	_playerPos = playerPos;
+	_slashNum = slashNum;
+	
+	_skillTimer = 0.f;
+	_destPos.x = playerPos->x;
+	_destPos.y = playerPos->y;
+
+
+	_totalFrameNum = END_TIME * 60;
+
+	_angle = atan2f(_destPos.y - _bossPos->y, _destPos.x - _bossPos->x);
+	
+	_vec.x = (_destPos.x - _bossPos->x) / _totalFrameNum;
+	_vec.y = (_destPos.y - _bossPos->y) / _totalFrameNum;
+
+	//_vec.x = MOVESPD * cosf(_angle);
+	//_vec.y = MOVESPD * sinf(_angle);
+
+}
--- chargeIceSlash.h
+++ chargeIceSlash.h
@@ -1,9 +1,52 @@
 #pragma once
 #include "gameNode.h"
+
+//	 보스 이동기
+//	플레이어가 있던 자리로 간다아아아아
+
+
+//	이닛 , 업데이트만 필요.
+//	렌더 필요없음
+//	
+
+
+
 class chargeIceSlash : public gameNode
 {
+private:
+
+	int _slashNum;
+	POINTFLOAT _destPos;
+	POINTFLOAT _vec;
+	float _angle;
+
+	float _skillTimer;
+	bool _isStart;
+
+	float _totalFrameNum;
+	
+	
+
+	POINTFLOAT* _bossPos;
+	POINTFLOAT* _playerPos;
+
+	const float END_TIME = 1.5f;
+	//const float MOVESPD = 20.f;
+
 public:
 	chargeIceSlash();
 	~chargeIceSlash();
+
+	HRESULT init();
+	void release();
+	void update();
+	void render();
+
+
+	//	slashNum 은 아무의미없다.
+	void UseSkill(POINTFLOAT* bossPos, POINTFLOAT* playerPos, int slashNum);
+
+
+
 };
 
