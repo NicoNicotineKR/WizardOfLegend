d9fa60611050e1f146a947e85dd4047a8ce3ea36
Grot1995 <pch8574@gmail.com>
2019-02-21 13:56:47 +0900

    보스방가가꼬 터지길래 고쳐봤십니더

--- chainLightning.cpp
+++ chainLightning.cpp
@@ -52,66 +52,71 @@ void chainLightning::update(player* Player)
 		_isSkill = false;
 		_reLoadCount = _maxReLoad;
 	}
-	if (10 > _attackCount)
+	if (_isSkill)
 	{
-		if ((*_vvMap)[_pos.y / 32][_pos.x / 32]->getIsAvailMove())
+		if (10 > _attackCount)
 		{
-			if (_imgCount > 0.05f)
+			if ((*_vvMap)[_pos.y / 32][_pos.x / 32]->getIsAvailMove())
 			{
-				_imgCount = 0;
-				if (_thunderFloorImg->getMaxFrameX() == _thunderFloorImg->getFrameX())
+				if (_imgCount > 0.05f)
 				{
-					_thunderFloorImg->SetFrameX(0);
-				}
-				if (_img->getMaxFrameX() == _img->getFrameX())
-				{
-					_img->SetFrameX(0);
-					if (_img->getFrameY() == 0)
+					_imgCount = 0;
+					if (_thunderFloorImg->getMaxFrameX() == _thunderFloorImg->getFrameX())
 					{
-						_img->SetFrameY(1);
+						_thunderFloorImg->SetFrameX(0);
 					}
-					else
+					if (_img->getMaxFrameX() == _img->getFrameX())
 					{
-						_img->SetFrameY(0);
+						_img->SetFrameX(0);
+						if (_img->getFrameY() == 0)
+						{
+							_img->SetFrameY(1);
+						}
+						else
+						{
+							_img->SetFrameY(0);
+						}
+						_pos.x = cosf(_angle) * 30 + _pos.x;
+						_pos.y = -sinf(_angle) * 30 + _pos.y;
+						//_collisionRc = RectMakeCenter(_pos.x + _img->getFrameWidth() / 2 - 135 / 2,
+						//	_pos.y + _img->getFrameHeight() - 25 - 900, 50, 50);
+
+						//	재만 추가 -- 충돌렉트 업데이트했으면, 맞았는지 검사해야게찌?
+						if (!_isHit) {
+							_collisionRc = RectMakeCenter(_pos.x + _img->getFrameWidth() / 2 - 135 / 2,
+								_pos.y + _img->getFrameHeight() - 25 - 900, 50, 50);
+							_em->RcCollideBySkillFunc(&_collisionRc, ATK_DMG, &_isHit);
+						}
+
+						_attackCount++;
+
 					}
-					_pos.x = cosf(_angle) * 30 + _pos.x;
-					_pos.y = -sinf(_angle) * 30 + _pos.y;
-					//_collisionRc = RectMakeCenter(_pos.x + _img->getFrameWidth() / 2 - 135 / 2,
-					//	_pos.y + _img->getFrameHeight() - 25 - 900, 50, 50);
-
-					//	재만 추가 -- 충돌렉트 업데이트했으면, 맞았는지 검사해야게찌?
-					if (!_isHit) {
-						_collisionRc = RectMakeCenter(_pos.x + _img->getFrameWidth() / 2 - 135 / 2,
-							_pos.y + _img->getFrameHeight() - 25 - 900, 50, 50);
-						_em->RcCollideBySkillFunc(&_collisionRc, ATK_DMG, &_isHit);
-					}
-
-					_attackCount++;
-
+					_img->SetFrameX(_img->getFrameX() + 1);
+					_thunderFloorImg->SetFrameX(_thunderFloorImg->getFrameX() + 1);
 				}
-				_img->SetFrameX(_img->getFrameX() + 1);
-				_thunderFloorImg->SetFrameX(_thunderFloorImg->getFrameX() + 1);
+			}
+			else
+			{
+				_attackCount = 10;
 			}
 		}
-		else
-		{
-			_attackCount = 10;
-		}
+
 	}
-
-
 
 }
 
 void chainLightning::render(player* Player)
 {
-	if (10 > _attackCount)
+	if (_isSkill)
 	{
-		if ((*_vvMap)[_pos.y / 32][_pos.x / 32]->getIsAvailMove())
+		if (10 > _attackCount)
 		{
-			_img->frameRender(Player->getPlayerMemDC(), _pos.x - (135 / 2) - CAMERA2D->getCamPosX(), _pos.y - 900 - CAMERA2D->getCamPosY());
-			_thunderFloorImg->frameRender(Player->getPlayerMemDC(), _pos.x-_thunderFloorImg->getFrameWidth()/2 - CAMERA2D->getCamPosX(),
-				_pos.y - _thunderFloorImg->getFrameHeight() / 2 - CAMERA2D->getCamPosY());
+			if ((*_vvMap)[_pos.y / 32][_pos.x / 32]->getIsAvailMove())
+			{
+				_img->frameRender(Player->getPlayerMemDC(), _pos.x - (135 / 2) - CAMERA2D->getCamPosX(), _pos.y - 900 - CAMERA2D->getCamPosY());
+				_thunderFloorImg->frameRender(Player->getPlayerMemDC(), _pos.x - _thunderFloorImg->getFrameWidth() / 2 - CAMERA2D->getCamPosX(),
+					_pos.y - _thunderFloorImg->getFrameHeight() / 2 - CAMERA2D->getCamPosY());
+			}
 		}
 	}
 }
